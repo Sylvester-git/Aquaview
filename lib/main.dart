0ae8f20e89c0a44e7c943710c6b1f102e36ec838
Nighttheone <142972168+Nighttheone@users.noreply.github.com>
2025-07-14 10:55:55 +0100

    Add a blank line for improved code readability in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ import 'package:waterapp/app/init.dart';
 import 'package:waterapp/config/config.dart';
 import 'package:waterapp/config/firebase_config.dart';
 
+
 void main() async {
   runZonedGuarded(
     () async {
